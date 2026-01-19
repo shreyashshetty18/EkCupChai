b796257eb185765bb6539bc4c593dcfe1f033bbc
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-01-19 08:14:03 +0530

    Files Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
   
   return (
-    <>
+    <Router>
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
       
-    </>
+    </Router>
   )
 }
 
