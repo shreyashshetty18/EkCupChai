0e354514f7995d4c15cdcf15a41693a751d89eea
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-01-28 22:12:52 +0530

    Hero Sec1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,19 @@
-const Hero = () => {
-  return null;
-};
+import React from 'react'
+import { FaStar } from 'react-icons/fa'
 
-export default Hero;
+const Hero = () => {
+  return (
+    <section>
+      <div className='relative z-50 flex flex-wrap justify-center items-center w-full max-w-3xl mx-auto px-2 sm:px-4 min-h-[calc(100vh-150px)]'>
+        {/* StarLine */}
+        <div className="flex flex-wrap items-center justify-center gap-1 text-sm font-medium text-chaiBrown mb-2 sm:mb-12">
+          {Array(5).fill(0).map((_, i) => (
+            <FaStar key={i} className="" />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
