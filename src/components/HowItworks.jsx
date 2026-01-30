c476e8f901b1b9a7b404cf8095c33abd1481344f
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-01-30 07:51:45 +0530

    HIW Sec1

--- src/components/HowItworks.jsx
+++ src/components/HowItworks.jsx
@@ -1,5 +1,15 @@
-const HowItWorks = () => {
-  return null;
-};
 
-export default HowItWorks;
+const  HowItWorks = () => {
+  return (
+    <section>
+      {/* Sub Heading */}
+      <p>Easy as Chai</p>
+      {/* Main Heading */}
+      <p>How It Works</p>
+      {/* Description */}
+      <p>Ek Cup Chai makes supporting creators fun and effortless. In just a few taps, your fans can send chai and show their appreciation.</p>
+    </section>
+  )
+}
+
+export default HowItWorks
