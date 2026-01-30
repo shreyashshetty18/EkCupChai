9c74cbe232d0a6fd522f94362381f612cc16e556
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-01-30 07:56:56 +0530

    HIW Sec Style

--- src/components/HowItworks.jsx
+++ src/components/HowItworks.jsx
@@ -1,13 +1,13 @@
 
 const  HowItWorks = () => {
   return (
-    <section>
+    <section className="py-8 px-4 sm:px-8 bg-[#f5f1ee] text-center relative z-10 overflow-hidden">
       {/* Sub Heading */}
-      <p>Easy as Chai</p>
+      <p className="text-xs text-gray-500 uppercase tracking-wide mb-2">Easy as Chai</p>
       {/* Main Heading */}
-      <p>How It Works</p>
+      <p className="text-3xl sm:text-5xl font-extrabold text-cahiBrown mb-6">How It Works</p>
       {/* Description */}
-      <p>Ek Cup Chai makes supporting creators fun and effortless. In just a few taps, your fans can send chai and show their appreciation.</p>
+      <p className="text-xs sm:text-lg text-gray-600 max-w-2xl mx-auto mb-16 sm:mb-20 px-8 sm:px-0">Ek Cup Chai makes supporting creators fun and effortless. In just a few taps, your fans can send chai and show their appreciation.</p>
     </section>
   )
 }
