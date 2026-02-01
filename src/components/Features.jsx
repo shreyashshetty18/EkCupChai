ea4371cf235b3eac68e948fbc5349d5c11e181b1
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-02-01 07:37:28 +0530

    Features 1

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,16 @@
-const Features = () => {
-  return null;
-};
+import React from 'react'
 
-export default Features;
+const Features = () => {
+  return (
+    <section>
+      {/* Sub Heading */}
+      <p>Creator-First-Platform</p>
+      {/* Main Heading */}
+      <h2>Why Creators Love Us</h2>
+      {/* Description */}
+      <p>We Built EkCupChai to help creators thrive and succeed in their journey. Here's what makes us special.</p>
+    </section>
+  )
+}
+
+export default Features
