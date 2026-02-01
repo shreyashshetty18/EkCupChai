61bbb64b132ad195598f4762e2b963c3affd2309
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-02-01 07:44:34 +0530

    Features Styling 1

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <section>
+    <section className='text-center py-2 px-4 sm:py-8 bg-[#f5f1ee] relative z-10 overflow-hidden'>
       {/* Sub Heading */}
-      <p>Creator-First-Platform</p>
+      <p className='text-xs text-gray-500 uppercase tracking-wide mb-2'>Creator-First-Platform</p>
       {/* Main Heading */}
-      <h2>Why Creators Love Us</h2>
+      <h2 className='text-2xl sm:text-5xl font-extrabold text-chaiBrown mb-6'>Why Creators Love Us</h2>
       {/* Description */}
-      <p>We Built EkCupChai to help creators thrive and succeed in their journey. Here's what makes us special.</p>
+      <p className='text-xs sm:text-lg text-gray-600 max-w-2xl mx-auto mb-16 sm:mb-20 px-8 sm:px-8'>We Built EkCupChai to help creators thrive and succeed in their journey. Here's what makes us special.</p>
     </section>
   )
 }
